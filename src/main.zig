a81e8e2315a085e9836a31100d6ff8e3d17bb59b
MichaelScofield45 <paco.villamonte@gmail.com>
2024-02-18 19:23:14 -0600

    feat: Expand directories, still wip

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const rl = @import("c.zig");
 const Entry = std.fs.Dir.Entry;
+const ArenaAllocator = std.heap.ArenaAllocator;
+const Allocator = std.mem.Allocator;
 
 const screen_width = 800;
 const screen_height = 600;
@@ -14,29 +16,30 @@ const DirEntry = struct {
 const FileEntry = [:0]const u8;
 
 const EntryList = struct {
-    allocator: std.mem.Allocator,
+    arena: ArenaAllocator,
     dirs: std.ArrayListUnmanaged(DirEntry),
     files: std.ArrayListUnmanaged(FileEntry),
+    children: std.StringHashMapUnmanaged(EntryList),
 
-    fn init(allocator: std.mem.Allocator) EntryList {
+    fn init(allocator: Allocator) EntryList {
         return .{
-            .allocator = allocator,
+            .arena = ArenaAllocator.init(allocator),
             .dirs = std.ArrayListUnmanaged(DirEntry){},
             .files = std.ArrayListUnmanaged(FileEntry){},
+            .children = std.StringHashMapUnmanaged(EntryList){},
         };
     }
 
     fn appendDir(self: *EntryList, name: [:0]const u8) !void {
-        try self.dirs.append(self.allocator, .{ .name = name, .open = false });
+        try self.dirs.append(self.arena.allocator(), .{ .name = name, .open = false });
     }
 
     fn appendFile(self: *EntryList, name: [:0]const u8) !void {
-        try self.files.append(self.allocator, name);
+        try self.files.append(self.arena.allocator(), name);
     }
 
     fn deinit(self: *EntryList) void {
-        self.dirs.deinit(self.allocator);
-        self.files.deinit(self.allocator);
+        self.arena.deinit();
     }
 
     fn getDirs(self: EntryList) []DirEntry {
@@ -48,23 +51,90 @@ const EntryList = struct {
     }
 };
 
-var arena: std.mem.Allocator = undefined;
+fn printList(list: *EntryList, x: f32, y: f32, mouse_pos: rl.Vector2) !f32 {
+    const arena = list.arena.allocator();
+    var yi = y;
+    for (list.getDirs()) |*dir| {
+        const arrow_pos = vec2(f32, x, yi);
+
+        const button = drawArrowV(arrow_pos, font_size - 5, dir.open);
+
+        rl.DrawText(
+            dir.name.ptr,
+            @intFromFloat(x + font_size),
+            @intFromFloat(yi),
+            font_size,
+            rl.DARKBLUE,
+        );
+
+        if (dir.open) {
+            const res = try list.children.getOrPut(arena, dir.name);
+            if (res.found_existing) {
+                const new_y = try printList(res.value_ptr, x + font_size, yi + font_size + 5, mouse_pos);
+                yi = new_y;
+            } else {
+                const path = try std.fs.path.join(arena, &.{ ".", dir.name });
+                res.value_ptr.* = try iterateDir(arena, path);
+            }
+        }
+
+        if (rl.IsMouseButtonPressed(rl.MOUSE_BUTTON_LEFT) and
+            rl.CheckCollisionPointRec(mouse_pos, button))
+        {
+            dir.open = !dir.open;
+            if (!dir.open) {
+                const match = list.children.getPtr(dir.name) orelse unreachable;
+                match.deinit();
+            }
+        }
+        yi += font_size;
+    }
+
+    for (list.getFiles()) |file| {
+        rl.DrawText(
+            file.ptr,
+            @intFromFloat(x),
+            @intFromFloat(yi),
+            font_size,
+            rl.LIGHTGRAY,
+        );
+        yi += font_size;
+    }
+
+    return yi - font_size / 2;
+}
+
+fn iterateDir(arena: Allocator, path: []const u8) !EntryList {
+    var list = EntryList.init(arena);
+    var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        const name = try arena.dupeZ(u8, entry.name);
+        switch (entry.kind) {
+            .directory => try list.appendDir(name),
+            .file => try list.appendFile(name),
+            else => |tag| std.log.info("encountered filetype '{s}'", .{@tagName(tag)}),
+        }
+    }
+
+    return list;
+}
 
 pub fn main() !void {
     rl.InitWindow(screen_width, screen_height, "filetree");
     rl.SetTargetFPS(60);
 
-    var arena_inst = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena_inst.deinit();
-    arena = arena_inst.allocator();
+    var gpa_inst = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa_inst.deinit();
+    const gpa = gpa_inst.allocator();
 
-    var list = EntryList.init(arena);
-
+    var list = EntryList.init(gpa);
+    defer list.deinit();
     {
         var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
         var iter = dir.iterate();
         while (try iter.next()) |entry| {
-            const name = try arena.dupeZ(u8, entry.name);
+            const name = try list.arena.allocator().dupeZ(u8, entry.name);
             switch (entry.kind) {
                 .directory => try list.appendDir(name),
                 .file => try list.appendFile(name),
@@ -73,46 +143,24 @@ pub fn main() !void {
         }
     }
 
+    var anchor = vec2(f32, 50, 50);
     while (!rl.WindowShouldClose()) {
         const m_pos = rl.GetMousePosition();
-        var start_pos = vec2(i32, 50, 50);
+        const start_x: f32 = anchor.x;
+        const start_y: f32 = anchor.y;
+
+        if (rl.IsKeyDown(rl.KEY_Q)) break;
+        if (rl.IsKeyDown(rl.KEY_UP)) anchor.y += 10;
+        if (rl.IsKeyDown(rl.KEY_DOWN)) anchor.y -= 10;
+        if (rl.IsKeyDown(rl.KEY_LEFT)) anchor.x += 10;
+        if (rl.IsKeyDown(rl.KEY_RIGHT)) anchor.x -= 10;
 
         rl.BeginDrawing();
 
         rl.ClearBackground(rl.RAYWHITE);
+        _ = try printList(&list, start_x, start_y, m_pos);
 
         rl.DrawFPS(0, 0);
-
-        for (list.getDirs()) |*dir| {
-            const arrow_pos = vec2(f32, start_pos.x - font_size, start_pos.y);
-
-            const button = drawArrowV(arrow_pos, font_size - 5, dir.open);
-            if (rl.IsMouseButtonPressed(rl.MOUSE_BUTTON_LEFT) and
-                rl.CheckCollisionPointRec(m_pos, button))
-            {
-                dir.open = !dir.open;
-            }
-
-            rl.DrawText(
-                dir.name.ptr,
-                @intFromFloat(start_pos.x),
-                @intFromFloat(start_pos.y),
-                font_size,
-                rl.DARKBLUE,
-            );
-            start_pos.y += font_size;
-        }
-
-        for (list.getFiles()) |file| {
-            rl.DrawText(
-                file.ptr,
-                @intFromFloat(start_pos.x),
-                @intFromFloat(start_pos.y),
-                font_size,
-                rl.LIGHTGRAY,
-            );
-            start_pos.y += font_size;
-        }
 
         rl.EndDrawing();
     }
